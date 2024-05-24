bfe57835dd3a3077ecb95a623a5e140da25c96c5
Natasha <40474308+Gitling01@users.noreply.github.com>
2024-05-24 12:45:29 -0400

    clean up

--- app/src/main/java/com/example/quakereport/Earthquake.java
+++ app/src/main/java/com/example/quakereport/Earthquake.java
@@ -1,63 +1,40 @@
 package com.example.quakereport;
 
 public class Earthquake {
-
     private double mMagnitude;
     private String mLocation;
     private long mTimeInMilliseconds;
 
-    /** Constructs a new Earthquake object.
+    /*
      * @param magnitude is the magnitude of the earthquake
      * @param location is the city location of the earthquake
      * @param timeInMilliseconds is the time in milliseconds (1/1/70)
      *  that the earthquake occurred
      */
     public Earthquake(double magnitude, String location, long timeInMilliseconds) {
-
         this.mMagnitude = magnitude;
         this.mLocation = location;
         this.mTimeInMilliseconds = timeInMilliseconds;
     }
 
-    /** Returns the magnitude of the earthquake */
-
-
-    public double getMagnitude()
-    {
+    public double getMagnitude(){
         return mMagnitude;
     }
 
-    /** Returns the city where the earthquake happened */
-
-
-    public String getLocation()
-    {
+    public String getLocation(){
         return mLocation;
     }
 
-    /** Returns the time of the earthquake */
-
-   public long getTimeInMilliseconds()
-   {
+   public long getTimeInMilliseconds(){
        return mTimeInMilliseconds;
    }
 
-
-    //setter methods follow
-
-    public void setMagnitude(double mag)
-    {
+    public void setMagnitude(double mag){
         this.mMagnitude = mag;
-
     }
 
-
-    public void setLocation(String location)
-    {
+    public void setLocation(String location){
       this.mLocation = location;
     }
-
-
-  //  public void setDate(long timeInMilliseconds) { this.mTimeInMilliseconds = timeInMilliseconds; }
 
 }
